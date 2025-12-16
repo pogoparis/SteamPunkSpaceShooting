b3660b3d4e4aeb41ce38cf23b9d7d421b61f1310
Pogoparis <pogoparis@gmail.com>
2025-12-16 08:36:02 +0100

    chore(core): start with LoadingScreen; dispose AssetService on shutdown

--- core/src/main/java/com/spaceshooter/game/SteamPunkSpaceShooter.java
+++ core/src/main/java/com/spaceshooter/game/SteamPunkSpaceShooter.java
@@ -1,11 +1,16 @@
 package com.spaceshooter.game;
-
+import com.spaceshooter.game.assets.AssetService;
 import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class SteamPunkSpaceShooter extends Game {
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+      setScreen(new LoadingScreen(this));
     }
+}
+@Override
+public void dispose() {
+    super.dispose();
+    AssetService.get().dispose();
 }
